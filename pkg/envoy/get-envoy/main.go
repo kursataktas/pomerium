c17bfa27337fdd8195248c52714520c7f7ee52d3
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2024-05-13 16:34:16 -0700

    WIP -- another option

--- pkg/envoy/get-envoy/main.go
+++ pkg/envoy/get-envoy/main.go
@@ -8,20 +8,29 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+
+	"golang.org/x/sync/errgroup"
 )
 
 const envoyVersion = "1.30.1"
 
 func main() {
 	dir := outputDir()
+	log.Println(dir)
+
+	var eg errgroup.Group
 	for _, target := range []string{
 		"darwin-amd64",
 		"darwin-arm64",
 		"linux-amd64",
 		"linux-arm64",
 	} {
-		download(target, dir)
+		download(&eg, target, dir)
 	}
+	if err := eg.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("done")
 }
 
 func outputDir() string {
@@ -34,17 +43,17 @@ func outputDir() string {
 	return filepath.Join(filepath.Dir(file), "../files")
 }
 
-func download(target, outputDir string) {
+func download(eg *errgroup.Group, target, outputDir string) {
 	url := fmt.Sprintf("https://github.com/pomerium/envoy-binaries/releases/download/v%s/envoy-%s",
 		envoyVersion, target)
 	outputPath := filepath.Join(outputDir, "envoy-"+target)
 
-	downloadIfNewer(url, outputPath)
-	downloadIfNewer(url+".sha256", outputPath+".sha256")
-	writeVersion(outputPath + ".version")
+	eg.Go(func() error { return downloadIfNewer(url, outputPath) })
+	eg.Go(func() error { return downloadIfNewer(url+".sha256", outputPath+".sha256") })
+	eg.Go(func() error { return writeVersion(outputPath + ".version") })
 }
 
-func downloadIfNewer(url, outputPath string) {
+func downloadIfNewer(url, outputPath string) error {
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("Accept", "*/*")
 	if fi, err := os.Stat(outputPath); err == nil {
@@ -53,31 +62,31 @@ func downloadIfNewer(url, outputPath string) {
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatalf("couldn't get %s: %v", url, err)
+		return fmt.Errorf("couldn't get %s: %w", url, err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusNotModified {
-		return
+		log.Printf("%s: not modified", url)
+		return nil
 	} else if resp.StatusCode != http.StatusOK {
-		log.Fatal("%s: %s", url, resp.Status)
+		return fmt.Errorf("%s: unexpected status %s", url, resp.Status)
 	}
 
 	log.Printf("downloading %s...\n", url)
 
 	f, err := os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	if _, err := io.Copy(f, resp.Body); err != nil {
-		log.Fatalf("couldn't download %s: %v", url, err)
+		return fmt.Errorf("couldn't download %s: %w", url, err)
 	}
+	log.Printf("downloadIfNewer %s done", url)
+	return nil
 }
 
-func writeVersion(outputPath string) {
-	output := make([]byte, 0, len(envoyVersion)+1)
-	output = append(output, []byte(envoyVersion)...)
-	output = append(output, byte('\n'))
-	if err := os.WriteFile(outputPath, output, 0o644); err != nil {
-		log.Fatal("couldn't write version: ", err)
-	}
+var versionFileContents = append([]byte(envoyVersion), byte('\n'))
+
+func writeVersion(outputPath string) error {
+	return os.WriteFile(outputPath, versionFileContents, 0o644)
 }
